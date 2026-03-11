7689da5a6e397b1829a6575d33488f975abf860d
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-03-11 18:02:42 +0530

    Update views.py

--- mm/views.py
+++ mm/views.py
@@ -422,6 +422,11 @@ def generate_form_a_pdf(request, training_id):
     c.setFont("Helvetica-BoldOblique", 10)
     #c.drawString(50, y, "* This certificate is valid only for the nature of job for which the training is imparted.")
     y -= line_gap
+	subsidiary_name = (
+    request.user.areas.first().subsidiary.subsidiary_name
+    if request.user.areas.exists() and request.user.areas.first().subsidiary
+    else "Unknown"
+	)
     c.drawString(50, y, f"* This certificate will have no claim for employment in {subsidiary_name}.")
     y -= line_gap
     year = {
@@ -559,6 +564,7 @@ def certificate_detail(request):
         }
 
     return render(request, 'mm/certificate_detail.html', context)
+
 
 
 
