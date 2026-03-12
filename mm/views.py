b322ee9ef67367124790863e0a99e1ec71024881
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-03-12 10:48:24 +0530

    Update views.py

--- mm/views.py
+++ mm/views.py
@@ -418,8 +418,12 @@ def generate_form_a_pdf(request, training_id):
     c.drawString(50, y, f"  * Blood Group - {worker.blood_group}")
    
 	# Footer note
-	
-	#y -= line_gap
+	y -= line_gap
+    c.setFont("Helvetica-BoldOblique", 10)
+    #c.drawString(50, y, "* This certificate is valid only for the nature of job for which the training is imparted.")
+    y -= line_gap
+    c.drawString(50, y, "* This certificate will have no claim for employment in NCL.")
+	y -= line_gap
     year = {
     "Basic": "5",
     "Refresher": "5"
@@ -555,6 +559,7 @@ def certificate_detail(request):
         }
 
     return render(request, 'mm/certificate_detail.html', context)
+
 
 
 
