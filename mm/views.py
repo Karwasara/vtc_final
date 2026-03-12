6a74a32570d12f3b2fcb870fea8c77c81028a7df
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-03-12 11:22:09 +0530

    Update views.py

--- mm/views.py
+++ mm/views.py
@@ -437,7 +437,7 @@ def generate_form_a_pdf(request, training_id):
 
     c.drawString(50, y, f"  * Blood Group - {worker.blood_group}")
 
-    y -= line_gap
+    y -= line_gap * 2
 
     c.setFont("Helvetica-BoldOblique", 10)
 
@@ -591,6 +591,7 @@ def certificate_detail(request):
         }
 
     return render(request, 'mm/certificate_detail.html', context)
+
 
 
 
