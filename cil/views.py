ad07c971142cb4ed51241af8929923f040b943e4
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-03-20 14:25:20 +0530

    Update views.py

--- cil/views.py
+++ cil/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render
 
-# Create your views here.
-from django.shortcuts import render, redirect
-from .forms import IndependentWorkerForm
-from .models import IndependentWorker
-from django.contrib import messages
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib import messages
-from .models import IndependentWorker
-from .forms import IndependentWorkerForm  # ensure this form exists
-from django.db.models import Max
+
 
 def dashboard(request):
     return render(request, 'cil/dashboard.html')
