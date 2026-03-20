ebfdea36a7c1567b36d815708d1767d3b8af304b
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-03-20 14:24:16 +0530

    Update urls.py

--- cil/urls.py
+++ cil/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path
+from mm import views as mm_views
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+app_name = 'cil'
 
+urlpatterns = [
+    
+    path('dashboard/', views.dashboard, name='dashboard'),
+   
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
