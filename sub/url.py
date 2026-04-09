c64220e20d63fcd25c2f03c5e6a81b23c76fdf0d
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-04-09 12:22:22 +0530

    Update url.py

--- sub/url.py
+++ sub/url.py
@@ -8,5 +8,7 @@ app_name = 'sub'
 urlpatterns = [
     
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('certificate/verify/', views.certificate_verification, name='certificate_verification'),
+    path('certificate-verification/', views.certificate_detail, name='certificate_detail'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
