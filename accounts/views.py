43b7702fe12b46ca6d07ee6233a58519d98f8744
Karwasara <94948480+Karwasara@users.noreply.github.com>
2026-06-02 11:59:33 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -15,7 +15,6 @@ from .forms import LoginForm
 
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
-@login_required(login_url='accounts:login')
 @never_cache
 def home(request):
     if request.user.is_authenticated:
